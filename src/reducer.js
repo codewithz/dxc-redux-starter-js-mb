6b56b9283bac3a5ca24c747336d14c395f80380d
Zartab Nakhwa <zartab@codewithz.com>
2022-03-14 12:54:37 +0530

    REDUX: Swithced to Swithc Case

--- src/reducer.js
+++ src/reducer.js
@@ -1,15 +1,19 @@
 
 let lastId = 5;
 function reducer(state, action) {
-    if (action.type === 'ADD_BUG') {
-        return [...state,
-        {
-            id: ++lastId,
-            description: action.payload.description,
-            resolved: false
-        }]
+    switch (action.type) {
+        case "ADD_BUG":
+            return [...state,
+            {
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            }];
+        case 'REMOVE_BUG':
+            return state.filter(bug => bug.id !== action.payload.id);
+        default:
+            return state;
+
     }
-    else if (action.type === 'REMOVE_BUG') {
-        return state.filter(bug => bug.id !== action.payload.id);
-    }
+
 }
